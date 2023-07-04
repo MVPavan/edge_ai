3b111a8b95e73d58270632161950833ba3a76332
mvpavan <mvpavan42@gmail.com>
2023-07-04 21:00:09 +0530

    testing github package

--- eai_deepstream/main.py
+++ eai_deepstream/main.py
@@ -3,7 +3,7 @@ from pipelines.pipeline_choices import ODSingleHead
 from pipelines.run_pipeline import run_pipeline
 
 if __name__=="__main__":
-    osd_single_head = ODSingleHead()
-    osd_single_head.build_pipeline()
-    run_pipeline(osd_single_head)
-    print("Finished Pipeline!")
+    # osd_single_head = ODSingleHead()
+    # osd_single_head.build_pipeline()
+    # run_pipeline(osd_single_head)
+    print("Ended Pipeline!")
