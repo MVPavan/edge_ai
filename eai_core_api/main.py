ce44b4a0bdb8f0aa6de5c3ee9c722afbaf7839fd
mvpavan <mvpavan42@gmail.com>
2023-07-11 16:22:51 +0530

    workflow to poetry

--- eai_core_api/main.py
+++ eai_core_api/main.py
@@ -1,10 +1,11 @@
 import uvicorn
 from backend.fastapi_app import fastapi_app
+from config import FASTAPI_HOST, FASTAPI_PORT
 
 # import test
 
 if __name__ == "__main__":
     print("Starting EAI Core API Server!!!")
-    uvicorn.run(fastapi_app, host="0.0.0.0", port=8090)
+    uvicorn.run(fastapi_app, host=FASTAPI_HOST, port=FASTAPI_PORT)
     
 
