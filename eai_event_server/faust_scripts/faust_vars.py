0ac0e79e85d884687658421bc929e6e2b7e3ef07
mvpavan <mvpavan42@gmail.com>
2023-07-30 20:22:04 +0530

    working on faust agent manager

--- eai_event_server/faust_scripts/faust_vars.py
+++ eai_event_server/faust_scripts/faust_vars.py
@@ -4,7 +4,7 @@ from imports import (
     faust,
 )
 
-from .faust_agent_manager import Agent
+from .faust_agent_manager import Agent, BusinessLogics
 
 worker_details = lambda app: f"{app.conf._id}:{app.conf.web_port}:{os.getpid()}"
 
@@ -12,13 +12,14 @@ class FaustAppCreateVars(BaseModel):
     faust_app_id: str
     broker: str
     pipeline_topic_id: str
-    business_logics: List[Agent]
+    business_logics: BusinessLogics
     sink_topic_ids: Optional[List[str]] = None
 
 class FaustAppVars(FaustAppCreateVars):
     app: faust.App
     pipeline_topic: Optional[faust.Topic] = None
     sink_topics: Optional[List[faust.Topic]] = None
+    business_logic_agents: List[Agent]
 
 
 
